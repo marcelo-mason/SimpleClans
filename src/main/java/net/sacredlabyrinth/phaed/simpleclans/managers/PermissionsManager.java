4a711cfa6097f7e1da79ed57e332c9aecb683eff
Phaed <phaedrus.one@gmail.com>
2015-03-23 13:24:05 -0500

    added back vault warning

--- src/main/java/net/sacredlabyrinth/phaed/simpleclans/managers/PermissionsManager.java
+++ src/main/java/net/sacredlabyrinth/phaed/simpleclans/managers/PermissionsManager.java
@@ -48,8 +48,7 @@ public final class PermissionsManager
         }
         catch (ClassNotFoundException e)
         {
-            //SimpleClans.log("[PreciousStones] Vault.jar not found. No economy support.");
-            //no need to spam everyone who doesnt use vault
+            SimpleClans.log("[PreciousStones] Vault not found. No economy or extended Permissions support.");
         }
     }
 
@@ -65,23 +64,26 @@ public final class PermissionsManager
 
     /**
      * Loads the permissions for each clan from the config
-     *
      */
-    public void loadPermissions() {
+    public void loadPermissions()
+    {
         SimpleClans.getInstance().getSettingsManager().load();
         permissions.clear();
-        for (Clan clan : plugin.getClanManager().getClans()) {
+        for (Clan clan : plugin.getClanManager().getClans())
+        {
             permissions.put(clan.getTag(), SimpleClans.getInstance().getConfig().getStringList("permissions." + clan.getTag()));
         }
     }
 
     /**
      * Saves the permissions for earch clan from the config
-     *
      */
-    public void savePermissions() {
-        for (Clan clan : plugin.getClanManager().getClans()) {
-            if (permissions.containsKey(clan.getTag())) {
+    public void savePermissions()
+    {
+        for (Clan clan : plugin.getClanManager().getClans())
+        {
+            if (permissions.containsKey(clan.getTag()))
+            {
                 SimpleClans.getInstance().getSettingsManager().getConfig().set("permissions." + clan.getTag(), getPermissions(clan));
             }
         }
@@ -90,30 +92,40 @@ public final class PermissionsManager
 
     /**
      * Adds all pemrissions for a clan
+     *
      * @param clan
      */
-    public void updateClanPermissions(Clan clan) {
-        for (ClanPlayer cp : clan.getMembers()) {
+    public void updateClanPermissions(Clan clan)
+    {
+        for (ClanPlayer cp : clan.getMembers())
+        {
             addPlayerPermissions(cp);
         }
     }
 
     /**
      * Adds permissions for a player
+     *
      * @param cp
      */
-    public void addPlayerPermissions(ClanPlayer cp) {
-        if (cp != null && cp.toPlayer() != null) {
+    public void addPlayerPermissions(ClanPlayer cp)
+    {
+        if (cp != null && cp.toPlayer() != null)
+        {
             Player player = cp.toPlayer();
-            if (permissions.containsKey(cp.getClan().getTag())) {
-                if (!permAttaches.containsKey(cp.toPlayer())) {
+            if (permissions.containsKey(cp.getClan().getTag()))
+            {
+                if (!permAttaches.containsKey(cp.toPlayer()))
+                {
                     permAttaches.put(cp.toPlayer(), cp.toPlayer().addAttachment(SimpleClans.getInstance()));
                 }
                 //Adds all permisisons from his clan
-                for (String perm : getPermissions(cp.getClan())) {
+                for (String perm : getPermissions(cp.getClan()))
+                {
                     permAttaches.get(cp.toPlayer()).setPermission(perm, true);
                 }
-                if (plugin.getSettingsManager().isAutoGroupGroupName()) {
+                if (plugin.getSettingsManager().isAutoGroupGroupName())
+                {
                     permAttaches.get(cp.toPlayer()).setPermission("group." + cp.getClan().getTag(), true);
                 }
                 player.recalculatePermissions();
@@ -123,26 +135,37 @@ public final class PermissionsManager
 
     /**
      * Removes permissions for a clan (when it gets disbanded for example)
+     *
      * @param clan
      */
-    public void removeClanPermissions(Clan clan) {
-        for (ClanPlayer cp : clan.getMembers()) {
-             removeClanPlayerPermissions(cp);
+    public void removeClanPermissions(Clan clan)
+    {
+        for (ClanPlayer cp : clan.getMembers())
+        {
+            removeClanPlayerPermissions(cp);
         }
     }
 
     /**
      * Removes permissions for a player (when he gets kicked for example)
+     *
      * @param cp
      */
-    public void removeClanPlayerPermissions(ClanPlayer cp) {
-        if (cp != null) {
-            if (cp.getClan() != null) {
-                if (cp.toPlayer() != null){
+    public void removeClanPlayerPermissions(ClanPlayer cp)
+    {
+        if (cp != null)
+        {
+            if (cp.getClan() != null)
+            {
+                if (cp.toPlayer() != null)
+                {
                     Player player = cp.toPlayer();
-                    if (player.isOnline()) {
-                        if (permissions.containsKey(cp.getClan().getTag())) {
-                            if(permAttaches.containsKey(player)) {
+                    if (player.isOnline())
+                    {
+                        if (permissions.containsKey(cp.getClan().getTag()))
+                        {
+                            if (permAttaches.containsKey(player))
+                            {
                                 permAttaches.get(player).remove();
                                 permAttaches.remove(player);
                             }
@@ -154,10 +177,11 @@ public final class PermissionsManager
     }
 
     /**
-     * @return the permissions for a clan
      * @param clan
+     * @return the permissions for a clan
      */
-    public List<String> getPermissions(Clan clan) {
+    public List<String> getPermissions(Clan clan)
+    {
         return permissions.get(clan.getTag());
     }
 
@@ -165,7 +189,8 @@ public final class PermissionsManager
     /**
      * @return the PermissionsAttachments for every player
      */
-    public HashMap<Player, PermissionAttachment> getPermAttaches() {
+    public HashMap<Player, PermissionAttachment> getPermAttaches()
+    {
         return permAttaches;
     }
 
